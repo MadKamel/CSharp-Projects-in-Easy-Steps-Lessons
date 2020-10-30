a6bcd3ce8fa130f74108aef198341b304ea87b92
Eric Rustrum <rustrumek@gmail.com>
2020-10-30 13:33:05 -0700

    Create main.cs

---
+++ Cast/main.cs
@@ -0,0 +1,28 @@
+using System;
+
+namespace Cast
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.Title = "Cast";
+
+			double num = 10.5;
+			int integer = 2;
+
+			num = num + integer;
+			Console.WriteLine("Implicit Cast: " + num);
+
+			num = ( double ) 7/integer;
+			Console.WriteLine("Explicit Cast: " + num);
+
+			char letter = ( char ) 65;
+			Console.WriteLine("Cast Integer: " + letter);
+
+			int ascii = ( int ) 'A';
+			Console.WriteLine("Cast Letter: " + ascii);
+			Console.ReadKey();
+		}
+	}
+}
